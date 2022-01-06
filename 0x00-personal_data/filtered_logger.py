3e0c82e619bf0b121d47ceaae3e58d58cad44caa
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-06 23:14:15 +0300

    documented

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separtor: str) -> str:
+    """ a filter that replaces fields with redaction in message"""
     for field in fields:
         message = re.sub(f'{field}=.+?{separtor}', f'{field}={redaction}{separtor}', message)
     return message

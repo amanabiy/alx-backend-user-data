2e18f4562a5e23d2d6822d7f931851963d32b519
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-06 23:00:27 +0300

    type annotated and fixed style

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """ print filtered message Module """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separtor):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separtor: str) -> str:
     """ a filter that replaces fields with redaction in message"""
     for field in fields:
-        message = re.sub(f'{field}=...{separtor}', f'{field}={redaction}{separtor}', message)
+        message = re.sub(f'{field}=...{separtor}',
+                         f'{field}={redaction}{separtor}', message)
     return message

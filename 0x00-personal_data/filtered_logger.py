11d13aa51d0cc430f4ea3a485f7a74e5dcf3424b
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-08 23:27:17 +0300

    fixed spelling error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,11 +6,11 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separtor: str) -> str:
+                 message: str, separator: str) -> str:
     """ a filter that replaces fields with redaction in message"""
     for field in fields:
-        message = re.sub(f'{field}=.+?{separtor}',
-                         f'{field}={redaction}{separtor}', message)
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
 
 

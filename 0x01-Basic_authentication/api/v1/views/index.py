ea434945f11551bf3a4d8c05ffa3bd558aeb7a44
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-11 00:00:30 +0300

    routes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'],  strict_slashes=False)
+def unauthorize() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the 401 error with abort
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the 403 error with abort
+    """
+    abort(403)

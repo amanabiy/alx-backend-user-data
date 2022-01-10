02eaa29d146ebcb6e52405778ea1c0bce353b774
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-10 23:58:59 +0300

    integrate basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,12 +6,51 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+AUTH_TYPE = getenv('AUTH_TYPE')
+
+# set up Authentication type based on env variable
+if AUTH_TYPE == 'auth':
+    auth = Auth()
+if AUTH_TYPE == 'basic_auth':
+    auth = BasicAuth()
+
+@app.before_request
+def before_request() -> str:
+    """ Autherize before request
+    """
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        pass
+    elif not auth.require_auth(request.path, paths):
+        pass
+    elif auth.authorization_header(request) is None:
+        abort(401)
+    elif auth.current_user(request) is None:
+        abort(403)
+    else:
+        pass
+        
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Not authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbiden handler
+    """
+    return jsonify({"error": "Forbidden"})
 
 
 @app.errorhandler(404)

0a91a6f6b04936589a8806aeb7bd20af23bb6fe7
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-12 00:15:30 +0300

    correct import

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,8 +6,8 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+# from api.v1.auth.auth import Auth
+# from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,9 +17,11 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 # set up Authentication type based on env variable
-if AUTH_TYPE == 'auth':
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-if AUTH_TYPE == 'basic_auth':
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 

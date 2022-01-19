473591b50240d8beddd3943f1fa31de3f8c2040a
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-19 17:19:23 +0300

    remove autoincrement

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,13 @@
-#!usr/bin/env python3
-""" User Model module
-"""
+
+#!/usr/bin/env python3
+""" User model """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
 class User(Base):
-    """ A user model
-    """
+    """ SQLAlchemy model for a database table named users """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
